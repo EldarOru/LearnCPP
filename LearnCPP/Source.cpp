9f20f80356d95f4c79c444cd44b67ac3bc8d2a80
EldarOru <macdog108@gmail.com>
2022-05-20 14:29:01 +0300

    new info

--- LearnCPP/Source.cpp
+++ LearnCPP/Source.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
+struct CustomPerson {
+	string name;
+	string surname;
+
+};
+
 int main() {
-	cout << "Hello world";
+	//cout << "Hello world";
+
+	// types
+	/*
+	int x = 5;
+	string hi = "string";
+	vector<int> nums = { 1, 3, 5, 7 };
+	CustomPerson customPerson = CustomPerson{ "hi", "hey" };
+	customPerson.name;
+	map<string, int> myMap;
+	myMap["one"] = 1;
+	*/
+
+	//cycles
+	
+	string s = "abcdefg";
+	for (char c : s) {
+		cout << c << ", ";
+	};
+
+	vector<int> mynums = {1,2,3,4,5,6};
+	for (auto c :mynums) {
+		cout << c << ", ";
+	};
+
+	int quantity = count(begin(mynums), end(mynums), 5);
+	cout << quantity << ", ";
+
+	sort(begin(mynums), end(mynums));
+
+	for (int i = 0; i<10; ++i) {
+		cout << i;
+	}
+	
+	//input and output 
+
+	string name;
+	cin >> name;
+	cout << "Hello, " << name;
 	return 0;
 }
